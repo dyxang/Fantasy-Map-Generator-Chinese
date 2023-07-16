87c4f7cb60e85dea3fa182a573ae851d09dc7818
Dyxang <justtestforanyone@foxmail.com>
2023-07-16 21:37:20 +0800

    还原更新内容

--- message.js
+++ message.js
@@ -1,9 +1,54 @@
 "use strict";
 
 // version and caching control
+const notifactionv = "2023.07.16.2121"; 
  // generator version, update each time
 
 {
+  const notifaction_Number = parseFloat(notifactionv);
+  const stored_notifactionv = localStorage.getItem("notifactionv") ? parseFloat(localStorage.getItem("notifactionv")) : 0;
+
+  const notifaction_isOutdated = stored_notifactionv !== notifaction_Number;
+  if (notifaction_isOutdated) clearCache();
+
+  const notifaction_showUpdate = stored_notifactionv < notifaction_Number;
+  if (notifaction_showUpdate) setTimeout(show_notifaction_Window, 60000);
+
+  function show_notifaction_Window() {
+    const changelog = "https://github.com/Azgaar/Fantasy-Map-Generator/wiki/Changelog";
+
+
+    alertMessage.innerHTML = /* html */ `<strong>更新时间：${notifactionv}</strong>
+<p><strong></strong><p>
+      <ul>
+修复破坏代码的汉化;润色汉化；修改错误汉化（不结合上下文后果）；还原部分英文
+      </ul>
+`;
+
+    const buttons = {
+      好的: function () {
+        $(this).dialog("close");
+        if (stored_notifactionv) localStorage.clear();
+        localStorage.setItem("notifactionv", notifactionv);
+      }
+    };
+
+    if (stored_notifactionv) {
+      buttons.Reload = () => {
+        localStorage.clear();
+        localStorage.setItem("notifactionv", notifactionv);
+        location.reload();
+      };
+    }
+
+    $("#alert").dialog({
+      resizable: false,
+      title: "汉化版公告栏",
+      width: "28em",
+      position: {my: "center center-4em", at: "bottom ", of: "svg"},
+      buttons
+    });
+  }
 
 
   async function clearCache() {
